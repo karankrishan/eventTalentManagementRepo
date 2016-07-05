712e17076036098f6e7632f1f2c5df5c08419d24
Karan Krishan <karan.krishan@gmail.com>
2016-07-05 22:53:56 +0530

    Latest addition for the prototype

--- eventTalentManagement/src/com/event/talent/management/client/EventTalentManagement.java
+++ eventTalentManagement/src/com/event/talent/management/client/EventTalentManagement.java
@@ -1,154 +1,66 @@
 package com.event.talent.management.client;
 
-import com.event.talent.management.shared.FieldVerifier;
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
+import com.extjs.gxt.ui.client.widget.Label;
+import com.extjs.gxt.ui.client.widget.Window;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+
 public class EventTalentManagement implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
+	@Override
+	public void onModuleLoad() {
+		final Window w = new Window();
+		w.setClosable(Boolean.FALSE);
+		w.setMinimizable(Boolean.FALSE);
+		w.setMaximizable(Boolean.FALSE);
+		w.setSize(250, 150);
+		w.setLayout(new FitLayout());
+		FlexTable panel = new FlexTable();
+		Label userNamelabel = new Label("Username");
+		final TextField<String> text = new TextField<String>();
+		text.setEmptyText("Enter your full name");
+		Label passwordlabel = new Label("Password");
+		final TextField<String> password = new TextField<String>();
+		password.setPassword(Boolean.TRUE);
+		password.setEmptyText("Password");
 
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {/*
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		panel.setWidget(0, 0, userNamelabel);
+		panel.setWidget(0, 1, text);
+		panel.setWidget(1, 0, passwordlabel);
+		panel.setWidget(1, 1, password);
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		// panel.add(password);
+		Button reset = new Button("Reset");
+		reset.addClickListener(new ClickListener() {
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
+			@Override
+			public void onClick(Widget sender) {
+				text.reset();
+				password.reset();
 			}
 		});
+		Button login = new Button("Login");
+		login.addClickListener(new ClickListener() {
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			*//**
-			 * Fired when the user clicks on the sendButton.
-			 *//*
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
+			@Override
+			public void onClick(Widget sender) {
+				RootPanel.get().remove(w);
+				new Layout().load();
 			}
-
-			*//**
-			 * Fired when the user types in the nameField.
-			 *//*
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			*//**
-			 * Send the name from the nameField to the server and wait for a response.
-			 *//*
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
-	*/
-	RootPanel.get().add(new ParentLayout());	
+		});
+		panel.setWidget(2, 0, reset);
+		panel.setWidget(2, 1, login);
+		w.add(panel);
+		RootPanel.get().add(w);
+		w.center();
 	}
 }
---
+++ eventTalentManagement/src/com/event/talent/management/shared/Desk.java
@@ -0,0 +1,14 @@
+package com.event.talent.management.shared;
+
+public class Desk extends TestClass {
+	
+	public Integer REST() {
+		System.out.println("WW");
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		TestClass t = new TestClass();
+		
+	}
+}
