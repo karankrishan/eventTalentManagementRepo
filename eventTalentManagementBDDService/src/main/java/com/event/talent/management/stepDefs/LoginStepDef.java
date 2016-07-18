7ede69cf1abc1aa900b196780ce864476250e709
Karan Krishan <karan.krishan@gmail.com>
2016-07-18 15:17:55 +0530

    adding spring configuration

--- eventTalentManagementBDDService/src/main/java/com/event/talent/management/stepDefs/LoginStepDef.java
+++ eventTalentManagementBDDService/src/main/java/com/event/talent/management/stepDefs/LoginStepDef.java
@@ -2,6 +2,9 @@ package com.event.talent.management.stepDefs;
 
 import junit.framework.Assert;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.event.talent.management.service.interfaces.ILoginService;
 import com.event.talent.management.service.interfaces.impl.LoginServiceImpl;
 import com.event.talent.management.shared.model.UserModel;
@@ -10,20 +13,29 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class Login {
+public class LoginStepDef {
+
+	private ApplicationContext applicationContext;
 	ILoginService loginService;
 	Boolean validate;
 
-	@Given("^I have chosen to log in.$")
-	public void I_have_chosen_to_log_in() throws Throwable {
-		loginService = new LoginServiceImpl();
+	public LoginStepDef() {
+		applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
+
 	}
 
-	@When("^I login with details.$")
-	public void I_login_with_details() throws Throwable {
+	@Given("^I have chosen to log in.$")
+	public void I_have_chosen_to_log_in() throws Throwable {
+		loginService = (LoginServiceImpl) applicationContext
+				.getBean("loginService");
+	}
+
+	@When("^I login with details. \"([^\"]*)\" as username and \"([^\"]*)\" as password.$")
+	public void I_login_with_details_as_username_and_as_password(
+			String userName, String pass) throws Throwable {
 		UserModel model = new UserModel();
-		model.setUsername("Karan");
-		model.setPassword("pass");
+		model.setUsername(userName);
+		model.setPassword(pass);
 		validate = loginService.validate(model);
 
 	}
